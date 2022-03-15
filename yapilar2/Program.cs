e784823ac3746871d88264e687215d16c48da041
Vimelia <58261422+Vimelia@users.noreply.github.com>
2022-03-15 04:19:03 +0300

    Update Program.cs

--- yapilar2/Program.cs
+++ yapilar2/Program.cs
@@ -1,7 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System;
 
-namespace YapilarSwitch
+namespace yapilar2
 {
     class Program{
         public static void Main(string[] args)
